b14bf4c657b4bf467a2d097f01d7d31b29ae075f
j.h.nogami <j.h.nogami@gmail.com>
2019-09-25 14:23:31 +0900

    Setup

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def login_form
+
+  end
+
+  def new
+
+  end
+
+  def logout
+
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get "/" => "home#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "about" => "home#about"
+
+  get "signup" => "user#new"
+  post "user/edit" => "user#edit"
+  get "login" => "user#login_form"
+  post "logout" => "users#logout"
+
+  get "posts/edit" => "posts#edit"
+  get "posts/new" => "posts#new"
+  get "posts/index" => "posts#index"
+  get "posts/show" => "posts#show"
+
+
+end
